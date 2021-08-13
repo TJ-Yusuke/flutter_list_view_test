bde4aaee58564d746ba1fcbca22215b91ce0f294
TJ-Yusuke <jeeproud@gmail.com>
2021-08-13 21:51:36 +0900

    add: タブレイアウトを実装

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,66 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      home: RandomWords(),
+      home: DefaultTabController(
+        length: choices.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("test app"),
+            bottom: TabBar(
+              tabs: choices.map((Choice choice) {
+                return Tab(
+                  text: choice.title,
+                  icon: Icon(choice.icon),
+                );
+              }).toList(),
+            ),
+          ),
+          body: TabBarView(
+            children: choices.map((Choice choice) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ChoiceCard(choice: choice),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Choice {
+  const Choice({required this.title, required this.icon});
+
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: "ランダム単語", icon: Icons.sort_by_alpha),
+  const Choice(title: "todoリスト", icon: Icons.list_alt),
+];
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key? key, required this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle? textStyle = Theme.of(context).textTheme.display1;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(choice.icon, size: 128.0, color: textStyle!.color),
+            Text(choice.title, style: textStyle),
+          ],
+        ),
+      ),
     );
   }
 }
